616cf2b60e36a548a6021284cc4b10ba85be223e
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-03-08 09:12:11 +0800

    用户表设计

--- user_srv/model/user.go
+++ user_srv/model/user.go
@@ -1,1 +1,19 @@
 package model
+
+import (
+	"time"
+)
+
+type BaseModel struct {
+	ID         int32     `gorm:"primarykey"`
+	CreateUser int32     `gorm:"column: create_user"`
+	UpdateUser int32     `gorm:"column: update_user"`
+	CreateTime time.Time `gorm:"column: create_time"`
+	UpdateTime time.Time `gorm:"column: update_time"`
+	IsDelete   bool
+}
+
+type User struct {
+	BaseModel
+	UserName string `gorm:"column: user_name;type:varchar(20);unique;index:index_user_name"`
+}
