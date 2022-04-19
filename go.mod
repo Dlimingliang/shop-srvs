3f939f99ddd243af27183961a793494a4b9f581f
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-04-19 09:19:15 +0800

    代码调整

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/fsnotify/fsnotify v1.5.1
 	github.com/golang/protobuf v1.5.2
 	github.com/hashicorp/consul/api v1.12.0
+	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.10.1
 	go.uber.org/zap v1.21.0
 	google.golang.org/grpc v1.45.0
--- user-srv/main.go
+++ user-srv/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
-	"go.uber.org/zap"
-	"net"
-
 	"github.com/hashicorp/consul/api"
+	"github.com/satori/go.uuid"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/Dlimingliang/shop-srvs/user-srv/global"
 	"github.com/Dlimingliang/shop-srvs/user-srv/handler"
@@ -53,7 +56,8 @@ func main() {
 	//生成注册对象
 	registration := new(api.AgentServiceRegistration)
 	registration.Name = global.ServerConfig.Name
-	registration.ID = global.ServerConfig.Name
+	serviceID := fmt.Sprintf("%s", uuid.NewV4())
+	registration.ID = serviceID
 	registration.Port = *port
 	registration.Address = address
 	//本地开发，暂时注释健康检查
@@ -68,8 +72,26 @@ func main() {
 	if err != nil {
 		zap.S().Panic("user-srv注册consul失败: " + err.Error())
 	}
-	err = server.Serve(listen)
-	if err != nil {
-		zap.S().Panic("grpc启动失败: " + err.Error())
-	}
+	go func() {
+		err = server.Serve(listen)
+		if err != nil {
+			zap.S().Panic("grpc启动失败: " + err.Error())
+		}
+	}()
+
+	//优雅终止
+	processed := make(chan struct{})
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+		<-c
+		err = client.Agent().ServiceDeregister(serviceID)
+		if err != nil {
+			zap.S().Error("user-srv注销失败")
+		}
+		zap.S().Info("user-srv注销成功")
+		close(processed)
+	}()
+	zap.S().Infof("user-srv启动成功,%s:%d", *ip, *port)
+	<-processed
 }
